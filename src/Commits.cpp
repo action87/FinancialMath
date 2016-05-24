6526467a59972b7ac445d18c7e319e43fd4b7744
action87 <vigh.gabor87@gmail.com>
2016-05-24 22:46:57 +0200

    Adding Test C++ file

---
+++ src/Commits.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Commits.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Gabor committed" << endl; // prints !!!Hello World!!!
+
+	int a;
+	cin >> a;
+	return 0;
+}
